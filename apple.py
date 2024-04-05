1a24a38bbea025e721615a043ac3c34e25cfb885
Henry Pickersgill <htap2@live.co.uk>
2024-04-05 18:40:32 +0100

    take fourier transform of individual peaks, add directories

--- apple.py
+++ apple.py
@@ -1,28 +1,49 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
-import wave
-import sys
 from scipy.io import wavfile
+from scipy.fft import rfft, rfftfreq
+import os
 
-samplerate, data = wavfile.read('0004_c2.wav')
+here = f'{os.path.dirname(os.path.realpath(__file__))}'
 
-peaks = []
-for i, val in enumerate(data):
-    if data[i] > 5000:
-        peaks.append(i)
+def separate_peaks(data):
+    peaks = []
+    for i, val in enumerate(data):
+        if data[i] > 5000:
+            peaks.append(i)
 
-peak_locs = []
-for i, val in enumerate(peaks):
-    try:
-        if peaks[i+1] > val + 5000:
+    peak_locs = []
+    for i, val in enumerate(peaks):
+        try:
+            if peaks[i+1] > val + 5000:
+                peak_locs.append(i)
+        except:
             peak_locs.append(i)
-    except:
-        peak_locs.append(i)
-        pass
+            pass
 
-for i in peak_locs:
-    plt.plot(data[peaks[i]-2500:peaks[i]+2500])
-    plt.savefig(f'test{i}.png')
+    peak_data = [
+        data[peaks[i] - 2500: peaks[i] + 2500] for i in peak_locs
+    ]
+
+    # plot data around peaks
+    for j, arr in enumerate(peak_data):
+        plt.plot(arr)
+        plt.savefig(f'{here}/peaks/rg_c4_peak{j}.png')
+    plt.close()
+
+    return peak_data
+
+samplerate, data = wavfile.read(f'{here}/recordings/rg_c4.wav')
+
+peak_data = separate_peaks(data)
+
+time = [i / samplerate for i in range(len(peak_data[0]))]
+
+for i in peak_data:
+    num_samples = len(i)
+    yf = rfft(i)
+    xf = rfftfreq(num_samples, 1 / samplerate)
+
+    plt.plot(xf, np.abs(yf))
     plt.show()
-
